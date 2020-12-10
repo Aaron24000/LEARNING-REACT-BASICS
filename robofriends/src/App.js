b6101919908e7ac31f5d6855d5806af3d347246c
Aaron24000 <aaron.noble@outlook.com>
2020-12-10 06:48:17 -0500

    Added some styling elements using CSS and a custom font that wasn't from Google for a change lol

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -2,16 +2,21 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
+import './App.css';
 
+//App component with 2 states
 class App extends Component {
+  //Any component that has state can use the constructor syntax function to create this.state
   constructor() {
     super();
+    //React uses this state to render and pass them down as props
     this.state = {
       robots: robots,
       searchfield: "",
     };
   }
-
+  //We manage the state in the code below. The app is the only thing that can change this state
+  //We passed down onSearchChange to the search box
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
 }
@@ -23,7 +28,7 @@ class App extends Component {
       });
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
